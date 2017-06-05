1bfe2fafb6b73e6338f454c3a973fecb9dc4db61
Alin Eugen Deac <ade@vestergaardcompany.com>
2017-06-05 13:11:22 +0200

    Export package's components

--- index.js
+++ index.js
@@ -3,3 +3,20 @@
 import mix from './src/mix';
 export default mix;
 
+// Declare mixin classes
+import DeclareMixin from './src/declare';
+export { DeclareMixin };
+
+// Decorators
+import BareMixin from './src/Decorators/BareMixin';
+export { BareMixin };
+
+import HasInstance from './src/Decorators/HasInstance';
+export { HasInstance };
+
+import Cached from './src/Decorators/Cached';
+export { Cached };
+
+// Utils
+import wrap from './src/Utils/wrap';
+export { wrap };
