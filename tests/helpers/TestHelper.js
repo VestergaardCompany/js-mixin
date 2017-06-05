8701f304b26ad9a7749b87408895e09b6a331a71
Alin Eugen Deac <ade@vestergaardcompany.com>
2017-06-05 13:26:07 +0200

    Change helper, add more make methods
    For the new dummy classes

--- tests/helpers/TestHelper.js
+++ tests/helpers/TestHelper.js
@@ -2,6 +2,8 @@
 
 import Box from './Classes/Box';
 import LargeBox from './Classes/LargeBox';
+import Screen from './Classes/Screen';
+import LargeScreen from './Classes/LargeScreen';
 
 /**
  * Test Helper
@@ -30,6 +32,26 @@ class TestHelper {
      */
     static makeRegularSubClass(){
         return new LargeBox();
+    }
+
+    /**
+     * Returns a new instance of a dummy class that
+     * uses decorated mixins
+     *
+     * @return {Screen}
+     */
+    static makeClassWithDecoratedMixins(){
+        return new Screen();
+    }
+
+    /**
+     * Returns new instance of a regular class that inherits
+     * from a superClass using decorated mixins
+     *
+     * @return {LargeScreen}
+     */
+    static makeSubClassThatInheritsFromDecorated(){
+        return new LargeScreen();
     }
 }
 
